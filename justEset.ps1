9fe7954458675b1fbe7042dd589b60f7962b8d09
murat <murat.balci@betek.com.tr>
2025-08-19 09:13:21 +0300

    just eset

---
+++ justEset.ps1
@@ -0,0 +1,34 @@
+param(
+    [string]$password
+)
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+
+function Uninstall-Product($productName, $passRequired = $true) {
+     $uninstall = gci "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" |
+            foreach { gp $_.PSPath } |
+            ? { $_ -match $productName } |
+            select UninstallString
+
+        if ($uninstall) {
+
+            $uninstallId = $uninstall.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
+            $uninstallId = $uninstallId.Trim()
+            
+            if ($passRequired -and $password) {
+                start-process "msiexec.exe" -arg "/X $uninstallId /qn /norestart password=$password" -Wait
+            }
+            else {
+                start-process "msiexec.exe" -arg "/X $uninstallId /qn /norestart" -Wait
+            }
+
+        }
+
+}
+
+
+Uninstall-Product "ESET Endpoint Security" $true
+Uninstall-Product "ESET Inspect Connector" $true
+Uninstall-Product "ESET Management Agent" $false
+Uninstall-Product "ESET Management Agent" $true
